6e7eebe57a7a832bbb36f6951bdd4b6b907eee95
Arman Rahman <armanrahman0@gmail.com>
2024-03-18 15:39:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# VueInvoice
+
+VueInvoice is an open-source invoicing tool built with Vue.js. It provides a simple and intuitive interface for generating invoices quickly and efficiently.
+
+## Features
+
+- Easily create invoices with customizable details.
+- Download invoices in PDF format.
+- User-friendly interface for a seamless experience.
+
+## Getting Started
+
+To get started with VueInvoice, follow these steps:
+
+1. Goto The Link: https://helloarman.github.io/vueinvoice
+2. Edit All Information.
+3. Add Row if you want to add more items.
+4. Click on Download after editing file.
+
+## Usage
+
+1. Edit invoice details such as client name, address, and itemized list.
+2. Click on the "Download" button to save the invoice as a PDF file.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests to help improve VueInvoice.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ script.js
@@ -0,0 +1,59 @@
+var app = Vue.createApp({
+    data(){
+        return {
+            brandColor: "#6366F1",
+            rowCount: 1,
+            rows: [{ amount: '', unit: '', total: 0 }],
+            subTotal: 0,
+            taxPercentage: '',
+            tax: 0,
+            total: 0,
+        }
+    },
+
+    computed: {
+        year() {
+            const currentDate = new Date();
+            return currentDate.getFullYear();
+        },
+    },
+    methods:{
+        addRow() {
+            this.rowCount++;
+
+            this.rows.push({ amount: '', unit: '', total: 0 });
+        },
+        removeRow(){
+            this.rowCount--;
+
+            this.rows.pop();
+            this.calculatePercentage();
+        },
+        calculateSubTotal(index) {
+            this.rows[index].total = this.rows[index].amount * this.rows[index].unit;
+
+            this.calculatePercentage();
+        },
+        calculatePercentage(){
+            this.subTotal = this.rows.reduce((acc, obj) => acc + obj.total, 0);
+            this.tax = (this.subTotal * (this.taxPercentage/100)).toFixed(3);
+            this.calculateTotal();
+        },
+        calculateTotal(){
+            this.total = parseInt(this.tax) + parseInt(this.subTotal);
+        },
+        download(){
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            })
+
+            setTimeout(function(){
+                const element = document.getElementById("invoice");
+                html2pdf().from(element).save("vueInvoice.pdf");
+            }, 2000);
+        }
+    }
+});
+
+app.mount('#app')
